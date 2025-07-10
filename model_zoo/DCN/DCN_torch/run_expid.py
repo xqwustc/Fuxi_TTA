7156f8e26c7bd7b8acaf594f1e2264503911d36e
Wang Xianquan <wxq2001@gmail.com>
2025-07-10 17:31:35 +0800

    <feat> update print

--- model_zoo/DCN/DCN_torch/run_expid.py
+++ model_zoo/DCN/DCN_torch/run_expid.py
@@ -19,6 +19,7 @@ import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+print(os.getcwd())
 import logging
 import sys
 import logging
