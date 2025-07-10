13aaf739015139461bb20758e01be0bac016cd65
Wang Xianquan <wxq2001@gmail.com>
2025-07-10 17:39:48 +0800

    <feat> update print sys path

--- model_zoo/DCN/DCN_torch/run_expid.py
+++ model_zoo/DCN/DCN_torch/run_expid.py
@@ -18,7 +18,7 @@
 import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
 print(f"Current sys.path: {sys.path}")
 import logging
 import sys
