953395fdcf787f45beb74cdeddc4b07d532c5f43
Wang Xianquan <wxq2001@gmail.com>
2025-07-10 17:12:22 +0800

    Enhance dataset and model configurations by adding frappe_x1 dataset details and DCN_frappe model template

--- model_zoo/DCN/DCN_torch/run_expid.py
+++ model_zoo/DCN/DCN_torch/run_expid.py
@@ -18,6 +18,9 @@
 import os
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+import logging
+import sys
 import logging
 import fuxictr_version
 from fuxictr import datasets
