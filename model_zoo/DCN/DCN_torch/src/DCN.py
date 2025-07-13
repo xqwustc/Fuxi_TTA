aaecf3dd4cac10aa180d20b3e41489745d613685
Wang Xianquan <wxq2001@gmail.com>
2025-07-13 19:33:33 +0800

    <feat> update mode

--- model_zoo/DCN/DCN_torch/src/DCN.py
+++ model_zoo/DCN/DCN_torch/src/DCN.py
@@ -46,10 +46,8 @@ class DCN(BaseModel):
         self.mode = kwargs.get('mode', None)
 
         # --- update for tayfs retrain start ---
-        if self.mode and self.mode == 'recon': # infer with the reconstructed embedding
-            self.embedding_layer = ReconEmbedding(feature_map, embedding_dim, method=self.method)
-        elif self.mode and self.mode == 'pre': # pre-training with the reconstructed embedding
-            self.embedding_layer = ReconEmbedding(feature_map, embedding_dim, method=self.method)
+        if self.mode and self.mode in ['recon', 'pre']: # infer with the reconstructed embedding
+            self.embedding_layer = ReconEmbedding(feature_map, embedding_dim, method=self.mode)
         else: # use the original embedding
             self.embedding_layer = FeatureEmbedding(feature_map, embedding_dim)
                                                             
